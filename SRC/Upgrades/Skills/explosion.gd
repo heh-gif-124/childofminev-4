dd7e7f4e73e3616219c7ecdcef143ae200a03d39
particlis <79189885+heh-gif-124@users.noreply.github.com>
2026-07-06 19:02:29 +0700

    qwefasfqwe

---
+++ SRC/Upgrades/Skills/explosion.gd
@@ -0,0 +1,11 @@
+extends Node
+class_name explosion
+var pattern = ["sq","tr","tr"]
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+func _activate():
+	get_parent().get_parent().hp += 15
+	get_tree().get_first_node_in_group("staff").energy -= 2
+	print("i work")
