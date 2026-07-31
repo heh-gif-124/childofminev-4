1fafe5becd68db5413b52b0a71cc4dc57ee619e4
heh-gif-124 <muhammadaffandaib124@gmail.com>
2026-07-31 10:12:15 +0700

    uiuuuuuuuuuu
    ef;kjjbbjkwe

--- SRC/Cores/Monoliths/Monolith_base.gd
+++ SRC/Cores/Monoliths/Monolith_base.gd
@@ -1,11 +1,16 @@
 extends Node2D
-
+@export var id := "HP"
 
 # Called when the node enters the scene tree for the first time.
 
 func _ready():
 	$Interact_handler.interacted.connect(func():
-		print("fatfuck")
+		if id == "HP":
+			var d = get_tree().get_first_node_in_group("Player")
+			d.max_hp += 10
+		elif id == "MP":
+			var d = get_tree().get_first_node_in_group("staff")
+			d.max_energy += 3
 	)
 
 
